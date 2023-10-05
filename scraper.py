4268f4a4fc1cea1b96346ad4a997eaa43568c44e
masonchester <110614975+masonchester@users.noreply.github.com>
2023-10-04 21:49:50 -0400

    added multithreadedness

--- scraper.py
+++ scraper.py
@@ -1,16 +1,19 @@
+from concurrent.futures import ThreadPoolExecutor
+from urllib.parse import urljoin
 from bs4 import BeautifulSoup
+from queue import Queue
 import requests
 
-# Using a set to store unique links and improve performance
-links = set()
-keywords = ["https", "http", "#cite"]
-def crawl(init_url, relative_url, depth):
+
+visited_urls = set([])
+thread_queue = Queue()
+
+keywords = ["wikipedia", "cite", "scryfall"]
+def crawl(url, depth):
     try:
-        full_url = init_url + relative_url
 
-        print(full_url)
-
-        response = requests.get(full_url, timeout=5)
+        print("Crawling: ",  url)
+        response = requests.get(url, timeout=5)
 
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, 'html.parser')
@@ -18,16 +21,30 @@ def crawl(init_url, relative_url, depth):
             if depth > 0:
                 for text in soup.find_all('p'):
                     for link in text.find_all('a'):
-                        new_relative_url = link.get("href")
+                        rel_url = link.get("href")
 
-                        # Check if the URL is relative and not already crawled
-                        if new_relative_url and new_relative_url not in links:
-                            if not any(i in new_relative_url for i in keywords):
-                                links.add(new_relative_url)
-                                crawl(init_url, new_relative_url, depth - 1)
+                        if rel_url and rel_url not in visited_urls:
+                            if not any(i in rel_url for i in keywords):
+                                new_url = urljoin(url, rel_url)
+                                thread_queue.put(new_url)
+                                visited_urls.add(new_url)
+                                crawl(new_url, depth - 1)
 
     except Exception as e:
         print("Error crawling website: " + full_url)
 
-init_url = "https://mtg.fandom.com"
-crawl(init_url, "/wiki/Portal:Story", 3)
+
+def threaded_crawl(url, depth, max_workers):
+    thread_queue.put(url)
+
+    executor = ThreadPoolExecutor(max_workers=max_workers)
+    while not thread_queue.empty():
+        current_url = thread_queue.get()
+        executor.submit(crawl,current_url, depth)
+
+
+url = "https://mtg.fandom.com/wiki/Portal:Story"
+threaded_crawl(url, depth=3, max_workers=5)
+
+
+
